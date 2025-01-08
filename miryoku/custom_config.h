f458f5490e0641c650561d8863f1195bcc6218b0
Pruthvi Raj N V <pruthvi.n.v@gmail.com>
2025-01-09 00:52:24 +0530

    Fix missing define for placeholder in custom keymap

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -2,6 +2,9 @@
 // https://github.com/manna-harbour/miryoku
 
 // This fixes adv360pro miryoku mapping, TODO: submit a patch to upstream to fix?
+
+#define XXX &none
+
 #define MIRYOKU_LAYOUTMAPPING_ADV360PRO( \
      K00, K01, K02, K03, K04,                                                        K05, K06, K07, K08, K09, \
      K10, K11, K12, K13, K14,                                                        K15, K16, K17, K18, K19, \
